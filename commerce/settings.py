3767ae61164200bb1d38dcdf1c2de7ac46cf8b15
Joy <122730271+joyinalgorithms@users.noreply.github.com>
2025-06-22 09:37:18 +0800

    Added allowed hosts

--- commerce/settings.py
+++ commerce/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['cs50w-auctions.onrender.com', 'localhost', '127.0.0.1']
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
